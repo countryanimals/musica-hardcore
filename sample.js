0dce6985e4e9242f462a15f2718f16e2239f513e
mondialanimals <bigiare41@gmail.com>
2025-10-31 18:32:15 +0100

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,20 @@
+{
+  "kicks": [
+    {"name": "Kick Distorto", "file": "kick1.wav", "type": "kick"},
+    {"name": "Kick Pesante", "file": "kick2.wav", "type": "kick"},
+    {"name": "Kick Acido", "file": "kick3.wav", "type": "kick"}
+  ],
+  "basses": [
+    {"name": "Bass Distorto", "file": "bass1.wav", "type": "bass"},
+    {"name": "Bass Hoover", "file": "bass2.wav", "type": "bass"}
+  ],
+  "synths": [
+    {"name": "Synth Acido", "file": "synth1.wav", "type": "synth"},
+    {"name": "Synth Hoover", "file": "synth2.wav", "type": "synth"}
+  ],
+  "fx": [
+    {"name": "FX Riser", "file": "fx1.wav", "type": "fx"},
+    {"name": "FX Downlifter", "file": "fx2.wav", "type": "fx"},
+    {"name": "FX Reverse", "file": "fx3.wav", "type": "fx"}
+  ]
+}
